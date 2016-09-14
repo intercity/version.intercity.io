185bb48afe6c41cee74b3a2d9861b05a22486a20
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-09-14 14:46:56 +0200

    Initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,60 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require "capybara/rails"
+require "active_support/testing/setup_and_teardown"
+require "sidekiq/testing"
+require 'minitest/mock'
+require "mocha/mini_test"
+require "minitest/reporters"
+require "database_cleaner"
+require 'capybara/poltergeist'
+
+Sidekiq::Testing.fake!
+
+DatabaseCleaner.clean_with :truncation
+DatabaseCleaner.strategy = :truncation
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in test/support/ and its subdirectories.
+Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
+
+class ActiveSupport::TestCase
+  fixtures :all
+
+  def self.check_for_valid_fixtures(name)
+    Object.const_get(name.to_s.camelize).all.each do |obj|
+      raise "Fixtures for #{name} should be valid - #{obj.errors.messages}" unless obj.valid?
+    end
+  end
+end
+
+class ActionController::TestCase
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  self.use_transactional_tests = false
+
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, js_errors: true)
+  end
+  Capybara.javascript_driver = :poltergeist
+
+  teardown do
+    DatabaseCleaner.clean
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+
+  def use_javascript
+    Capybara.current_driver = Capybara.javascript_driver
+  end
+
+  def wait_for_ajax
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      loop until page.evaluate_script('jQuery.active').zero?
+    end
+  end
+end
