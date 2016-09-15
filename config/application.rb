98f0aa68285ce12e89a634fafc5b379b7399d83f
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-09-15 12:02:47 +0200

    Ensure we use sidekiq for active_job

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module IntercityVersion
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Europe/Amsterdam"
+
+    config.active_job.queue_adapter     = :sidekiq
   end
 end
